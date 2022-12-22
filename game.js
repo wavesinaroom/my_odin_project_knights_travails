29828e411b7dc150d0b62b5c6adf7d6f91547a2c
wavesinaroom <juan@wavesinaroom.com>
2022-12-22 15:24:37 -0500

    [Feature] Place a knight (k letter) in the middle of the keyboard

--- game.js
+++ game.js
@@ -1,14 +1,26 @@
 class Board{
   size = 8;
+
   constructor(){
-    this.tiles = Array(this.size).fill(Array(this.size).fill('x'));
+    this.tiles = Array.from(Array(this.size), ()=>Array.from(Array(this.size))); 
+    for(let r = 0; r<this.size; ++r)
+      for(let c = 0; c<this.size; ++c)
+        this.tiles[r][c] = ',';
   }
 
   visualize(){
-    this.tiles.forEach(tile=>console.log(tile.join(' ')));
+    this.tiles.forEach(row=>console.log(row.join(' ')));
+  }
+
+  placeKnight(row, col){
+    this.tiles[row][col] = 'k';
   }
 }
 
+class Knight{
+  ascii = 'k';
+  position = [];
+}
 const board = new Board();
-
+board.placeKnight(4, 4);
 board.visualize();
