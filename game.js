ecb8511ef05015ca16af90cb6e12b3371c653bcb
wavesinaroom <juan@wavesinaroom.com>
2022-12-23 18:11:57 -0500

    [Task] Test each move in getMoves() method

--- game.js
+++ game.js
@@ -1,20 +1,21 @@
 class Board{
-  size = 8;
+  static size = 8;
+
   targetPos = [];
   targetToken = 'd';
   moveToken = 'a';
 
   static isOutOfBoundaries(x,y){
-    if(x>this.size-1||x<0)
+    if(x>Board.size-1||x<0)
       return true;
-    else if(y>this.size-1||y<0)
+    else if(y>Board.size-1||y<0)
       return true;
     else
-      return false
+      return false;
   }
 
   constructor(){
-    this.tiles = Array.from(Array(this.size), ()=>Array.from(Array(this.size), tile=>'·')); 
+    this.tiles = Array.from(Array(Board.size), ()=>Array.from(Array(Board.size), tile=>'·')); 
   }
 
   visualize(){
@@ -28,7 +29,7 @@ class Board{
       this.tiles[position[1]][position[0]] = token;
   }
   
-  showPieceMoves(piece){
+  markPieceMoves(piece){
     for(let i = 0; i<piece.moves.length; ++i)
       this.tiles[piece.moves[i][1]][piece.moves[i][0]] = this.moveToken;
   }
@@ -46,56 +47,53 @@ class Knight{
     const twoStep = 2;
     const oneStep = 1;
    
-    //Check if moves are allowed before creating branched moves from each of them
-
     // -
     //|
     //|
-    if(!Board.isOutOfBoundaries(this.position[0]+oneStep,this.position[1]+twoStep))
-      this.moves.push(Array.from([this.position[0]+oneStep,this.position[1]+twoStep]));
-
-    //  |
-    //--
-    if(!Board.isOutOfBoundaries(this.position[0]+twoStep,this.position[1]+oneStep))
-      this.moves.push(Array.from([this.position[0]+twoStep,this.position[1]+oneStep]));
-
-    //--
-    //  |
-    if(!Board.isOutOfBoundaries(this.position[0]+twoStep,this.position[1]-oneStep))
-      this.moves.push(Array.from([this.position[0]+twoStep,this.position[1]-oneStep]));
-
-    //|
-    //|
-    // -
     if(!Board.isOutOfBoundaries(this.position[0]+oneStep,this.position[1]-twoStep))
       this.moves.push(Array.from([this.position[0]+oneStep,this.position[1]-twoStep]));
 
+    //  |
+    //--
+    if(!Board.isOutOfBoundaries(this.position[0]+twoStep,this.position[1]-oneStep))
+      this.moves.push(Array.from([this.position[0]+twoStep,this.position[1]-oneStep]));
+
+    //--
+    //  |
+    if(!Board.isOutOfBoundaries(this.position[0]+twoStep,this.position[1]+oneStep))
+      this.moves.push(Array.from([this.position[0]+twoStep,this.position[1]+oneStep]));
+
+    //|
+    //|
+    // -
+    if(!Board.isOutOfBoundaries(this.position[0]+oneStep,this.position[1]+twoStep))
+      this.moves.push(Array.from([this.position[0]+oneStep,this.position[1]+twoStep]));
+
     // |
     // |
     //-
-    if(!Board.isOutOfBoundaries(this.position[0]-oneStep,this.position[1]-twoStep))
-      this.moves.push(Array.from([this.position[0]-oneStep,this.position[1]-twoStep]));
-
-    // --
-    //|
-    if(!Board.isOutOfBoundaries(this.position[0]-twoStep,this.position[1]-oneStep))
-      this.moves.push(Array.from([this.position[0]-twoStep,this.position[1]-oneStep]));
-    //
-    //|
-    // --
-    if(!Board.isOutOfBoundaries(this.position[0]-twoStep,this.position[1]+oneStep))
-      this.moves.push(Array.from([this.position[0]-twoStep,this.position[1]+oneStep]));
-
-    //-
-    // |
-    // |
     if(!Board.isOutOfBoundaries(this.position[0]-oneStep,this.position[1]+twoStep))
       this.moves.push(Array.from([this.position[0]-oneStep,this.position[1]+twoStep]));
+
+    // --
+    //|
+    if(!Board.isOutOfBoundaries(this.position[0]-twoStep,this.position[1]+oneStep))
+      this.moves.push(Array.from([this.position[0]-twoStep,this.position[1]+oneStep]));
+    
+    //|
+    // --
+    if(!Board.isOutOfBoundaries(this.position[0]-twoStep,this.position[1]-oneStep))
+      this.moves.push(Array.from([this.position[0]-twoStep,this.position[1]-oneStep]));
+
+    //-
+    // |
+    // |
+    if(!Board.isOutOfBoundaries(this.position[0]-oneStep,this.position[1]-twoStep))
+      this.moves.push(Array.from([this.position[0]-oneStep,this.position[1]-twoStep]));
   }
 }
 const board = new Board();
-const knight = new Knight(3,5);
+const knight = new Knight(3,0);
 board.placeItem(knight.position, knight.token);
 knight.getMoves();
-board.showPieceMoves(knight);
 board.visualize();
