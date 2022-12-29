b41f23819f59b8c20533cd737225dda34f116a88
wavesinaroom <juan@wavesinaroom.com>
2022-12-29 17:49:28 -0500

    [Task] Remove code that's out of scope

--- game.js
+++ game.js
@@ -78,26 +78,6 @@ class MovesTree{
 
   }
 
-  findTargetNode(target, visited){
-    if((this.uur!==undefined)&&(this.uur.pos.toString() === target.toString())&&(this.urr.pos.toString()!==visited.pos.toString()))
-      return this.uur;
-    else if((this.rru!==undefined)&&(this.rru.pos.toString() === target.toString())&&this.rru.pos.toString()!==visited.pos.toString())
-      return this.rru;
-    else if((this.rrd!==undefined)&&(this.rrd.pos.toString() === target.toString())&&this.rrd.pos.toString()!==visited.pos.toString())
-      return this.rrd;
-    else if((this.ddr!==undefined)&&(this.ddr.pos.toString() === target.toString())&&this.ddr.pos.toString()!==visited.pos.toString())
-      return this.ddr;
-    else if((this.ddl!==undefined)&&(this.ddl.pos.toString() === target.toString())&&this.ddl.pos.toString()!==visited.pos.toString())
-      return this.ddl;
-    else if((this.lld!==undefined)&&(this.lld.pos.toString() === target.toString())&&this.lld.pos.toString()!==visited.pos.toString())
-      return this.lld;
-    else if((this.llu!==undefined)&&(this.llu.pos.toString() === target.toString())&&this.llu.pos.toString()!==visited.pos.toString())
-      return this.llu;
-    else if((this.uul!==undefined)&&(this.uul.pos.toString() === target.toString())&&this.uul.pos.toString()!==visited.pos.toString())
-      return this.uul;
-    else
-      return undefined;
-  }
 }
 class Knight{
   token = 'k';
@@ -106,7 +86,7 @@ class Knight{
     this.moves = new MovesTree(this.position);
   }
 
-  getMoves(root,target, visited){
+  getMoves(root){
     const twoSteps = 2;
     const oneStep = 1;
     
@@ -154,36 +134,12 @@ class Knight{
     if(!Board.isOutOfBoundaries(root.pos[0]-oneStep,root.pos[1]-twoSteps))
       root.uul=new MovesTree([root.pos[0]-oneStep,root.pos[1]-twoSteps]);
 
-    const foundTarget = root.findTargetNode(target, root);
-    console.log(foundTarget);
-    console.log(`Root:${root.pos} Visited:${visited.pos}`)
-
-    if(foundTarget!==undefined)
-      console.log('found')
-
-    if(root.uur!==foundTarget)
-      console.log(root.uur.pos)
-    if(root.rru!==foundTarget)
-      console.log(root.rru.pos)
-    if(root.rrd!==foundTarget)
-      console.log(root.rrd.pos)
-    if(root.ddr!==foundTarget)
-      console.log(root.ddr.pos)
-    if(root.ddl!==foundTarget)
-      console.log(root.ddl.pos)
-    if(root.lld!==foundTarget)
-      console.log(root.lld.pos)
-    if(root.llu!==foundTarget)
-      console.log(root.llu.pos)
-    if(root.uul!==foundTarget)
-      console.log(root.uul.pos)
-        
   }    
 
 }
 const board = new Board();
 board.targetPos = [5,2];
-const knight = new Knight(3,3);
+const knight = new Knight(2,3);
 
 board.placeItem(knight.position, knight.token);
 board.placeItem(board.targetPos, board.targetToken)
